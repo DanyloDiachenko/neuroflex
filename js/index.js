ae504f634f5c39a78380458e7b11d59c772692f5
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-07-30 12:34:19 +0300

    slider test

--- js/index.js
+++ js/index.js
@@ -22,3 +22,45 @@ document.addEventListener("click", (event) => {
         navigationMenu.classList.remove("active");
     }
 });
+
+/* Swiper (Slider) */
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 4,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    on: {
+        init: updateSlidesScale,
+        slideChangeTransitionStart: updateSlidesScale,
+        slideChangeTransitionEnd: updateSlidesScale,
+    },
+});
+
+function updateSlidesScale() {
+    let allSlides = document.querySelectorAll(".swiper-slide");
+    allSlides.forEach((slide) => {
+        slide.style.height = "414px";
+        slide.style.opacity = "0.5";
+    });
+
+    let activeSlide = document.querySelector(".swiper-slide-active");
+    if (activeSlide) {
+        activeSlide.style.height = "828px";
+        activeSlide.style.opacity = "1";
+
+        let nextSlide = activeSlide.nextElementSibling;
+        let prevSlide = activeSlide.previousElementSibling;
+        if (nextSlide) {
+            nextSlide.style.height = "628px";
+            nextSlide.style.opacity = "0.75";
+        }
+        if (prevSlide) {
+            prevSlide.style.height = "628px";
+            prevSlide.style.opacity = "0.75";
+        }
+    }
+}
